37737d521070c480d11a147a08efe9ac5451bf39
maqingbo <wmaqingbo@163.com>
2019-05-13 21:35:01 +0800

    feat: apiUrl 统一管理

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+const api = {
+  home: {
+    swiperImgs: [{
+      url: '../../static/swiper/swiper_01.jpg'
+    }, {
+      url: '../../static/swiper/swiper_02.jpg'
+    }, {
+      url: '../../static/swiper/swiper_03.jpg'
+    }, {
+      url: '../../static/swiper/swiper_04.jpg'
+    }]
+  }
+}
+
+export {
+  api
+}
